06468c3908a4a9efc5b5fedf0ecd2e1f88e9c452
deshanharindu2008 <109273468+deshanharindu2008@users.noreply.github.com>
2022-07-14 13:57:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,23 +19,23 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is DESHAN BOT🔥
+My Creator is Kumuthu DESHAN😎` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94777495774'] //Owner number in aive msg
+global.premium = ['+94777495774'] //Owner Number info
+global.ownernomer = '+94777495774' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
-global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
+global.ownername = '😎DESHAN😎' //Owner Name
+global.botname = ' DESHAN BOT🔥' //Bot Name
 
 global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.btnurl = 'https://youtube.com/channel/UCk8PxE9yXmlOiJ9RqSOot3Q' //Change Your Button's URL
 
-global.region = 'India, South India, Kerala' //Country Region
+global.region = 'SRI LANKA😎' //Country Region
 
-global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.packname = 'DESHAN BOT🔥' //Bot Pacage Name
+global.author = '😎DESHAN😎' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
@@ -43,7 +43,7 @@ global.dragonemoji = '🧩' // Menu emoji
 global.footer = '©_Kumuthu.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
-global.ig = 'https://github.com/prabhasha2006' //Github Account!
+global.ig = 'https://github.com/deshanharindu2008' //Github Account!
 
 global.mess = { // COSTOMIZE WITH YOUR LANGUAGE
 	
